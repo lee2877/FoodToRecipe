59ba935cd47f2cbef59a57e1689b6550e9e07b09
TaehoonKim <lazygame05@gmail.com>
2020-04-29 15:47:50 -0400

    Ranking.js -> appear the list when click GetRank twice on the top.top

--- src/components/Ranking.js
+++ src/components/Ranking.js
@@ -15,7 +15,7 @@ class Ranking extends Component{
         this.handleChange = this.handleChange.bind(this);
 
         this.state = {
-            recipes: ["test"],
+            recipes: ["Here"],
             foods: [],
             likes: 0,
             favRecipes: [],
@@ -69,11 +69,14 @@ class Ranking extends Component{
 
     getRank(){
         var arr = new Array();
+        var temparr = ["1"];
      
-        console.log("recipe "+ this.props.recipe);
+        console.log("recipe "+ this.props.recipes);
         var rankRec = fire.database().ref('/recipes/');
         rankRec.orderByChild('likes').limitToLast(10).on("child_added",snap => {
+            
             arr.push(snap.key.toString());
+            // temparr.push(snap.key.toString());
             console.log(snap.key + ' Likes ' + snap.val().likes);
             //console.log(snap);
         })
@@ -82,12 +85,35 @@ class Ranking extends Component{
             // console.log(data.val());
         // })
 
-        // var arr2 = ["1", "2"];
+        var arr2 = ["1", "2"];
+        console.log("hahaha");
+        console.log(arr);
+        console.log(arr[1]);
+        var arr3 = new Array();
+        
+        arr3.push("10: "+arr[0]);
+        arr3.push("9:  "+arr[1]);
+        arr3.push("8:  "+arr[2]);
+        arr3.push("7:  "+arr[3]);
+        arr3.push("6:  "+arr[4]);
+        arr3.push("5:  "+arr[5]);
+        arr3.push("4:  "+arr[6]);
+        arr3.push("3:  "+arr[7]);
+        arr3.push("2:  "+arr[8]);
+        arr3.push("1: "+ arr[9]);
 
-        console.log(arr.key);
-        // console.log(arr2);
 
-        this.setState({ recipes: arr.key });
+        // arr.keys;
+        // // console.log("!hohoho?");
+        // console.log("newARr" + arr3);
+        //console.log(arr3[1]);
+        // console.log(temparr.pop());
+
+        
+        this.setState({ recipes: arr3 });
+
+        // console.log("-------------------------------------");
+        // console.log(this.state.recipes);
     }
 
     handleGoBack =()=>{
@@ -100,10 +126,20 @@ class Ranking extends Component{
        
                 <button className="btn btn-info Btn" type="submit" onClick={this.handleGoBack} >Go Back</button>
                 <button className="btn btn-info Btn" type="submit" onClick={this.getRank} >GetRank</button>
-
-                <div className="recipe-list">
-                    {this.state.recipes}
+                <div>
+                <div class='recipe-list'>{this.state.recipes[9]}</div>
+                <div class='recipe-list'>{this.state.recipes[8]}</div>
+                <div class='recipe-list'>{this.state.recipes[7]}</div>
+                <div class='recipe-list'>{this.state.recipes[6]}</div>
+                <div class='recipe-list'>{this.state.recipes[5]}</div>
+                <div class='recipe-list'>{this.state.recipes[4]}</div>
+                <div class='recipe-list'>{this.state.recipes[3]}</div>
+                <div class='recipe-list'>{this.state.recipes[2]}</div>
+                <div class='recipe-list'>{this.state.recipes[1]}</div>
+                <div class='recipe-list'>{this.state.recipes[0]}</div>
                 </div>
+
+
 
                 <div class="BottomBar2">
                   Copyright by Haeun Lee, Brian Long, Taehoon Kim (2020)
